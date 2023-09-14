e6e95f26d2232e3967e48ee35ddb0b55db74a17e
Marc raphael <marcraphael979@gmail.com>
2023-09-15 00:35:08 +0100

    config/routes: Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   get '/foods', to: 'foods#index', as: 'user_root'
   resources :foods, only: [:index]
-  resources :recipes, only: [:index, :show]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy]
   resources :public_recipes, only: [:index]
   resources :general_shopping_list, only: [:index]
   root "foods#index"
