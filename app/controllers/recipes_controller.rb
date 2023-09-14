c4f60c7fc9f1bd9b5c44e0f9b0ad84fafdee4def
Marc raphael <marcraphael979@gmail.com>
2023-09-15 00:18:13 +0100

    app/controllers/recipes_controller: Update indes action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,13 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    # checking if the `current_user` is
+    # truthy. If it is truthy, it means that the user is currently logged in. In that case, the code
+    # will not execute the `redirect_to` method.
+    redirect_to current_user_path unless current_user
 
-  def show; end
+    @recipes = current_user.recipes
+  end
+
+  def show
+  end
 end
