8c72834c195128b2520418941983931abbf52851
Moyasi Ginko <mrsiyam61@gmail.com>
2023-09-15 22:10:00 +0600

    Add food list

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,10 +1,23 @@
 class FoodsController < ApplicationController
+  # Other actions
+
   def index
+    @food = Food.new
+    @foods = current_user.foods
+  end
+
+  def create
     @food = current_user.foods.new(food_params)
     if @food.save
       redirect_to foods_path, notice: 'Great, you have a new food!'
     else
-      redirect_to foods_path, notice: 'Food not saved!'
+      render :index
     end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :description, :other_attributes)
   end
 end
