d4dbe7c0ffd96ad768e7e59c6d69e6f33fd27d83
Moyasi Ginko <mrsiyam61@gmail.com>
2023-09-16 01:37:35 +0600

    Implement delete button for food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -17,6 +17,12 @@ class FoodsController < ApplicationController
     end
   end
 
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path, notice: 'Food item deleted.'
+  end
+
 
   private
 
