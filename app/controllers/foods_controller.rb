11147696938ac9a029a0bd90f639f9cb82eac781
Marc raphael <marcraphael979@gmail.com>
2023-09-15 09:34:54 +0100

    app: Upate food model

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,10 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @food = current_user.foods.new(food_params)
+    if @food.save
+      redirect_to foods_path, notice: 'Great, you have a new food!'
+    else
+      redirect_to foods_path, notice: 'Food not saved!'
+    end
+  end
 end
